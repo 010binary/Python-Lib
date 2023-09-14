99b63d2a0f2397ade95140e075740386e56e660b
010binary <augustine32g@gmail.com>
2023-09-14 06:34:57 +0100

    restructuring

---
+++ Classes and function/class_func.py
@@ -0,0 +1,9 @@
+def value ():
+    pass
+
+class Cat: # this is a class but is of the type -> type 
+    def __init__(self):
+        pass
+
+print(type(value))
+print(type(Cat))
---
+++ Data types/data.py
@@ -0,0 +1,20 @@
+# Types of data in Python 
+
+#Strings
+string ="This is a string" 
+
+#Integer or whole numbers
+integer = 98
+
+#Floats or Decimal numbers
+floats = 50.8
+
+#Boolean/ True and False with are used for decision.
+boolean1 = True
+boolean2 = False
+
+print(type(string))
+print(type(integer))
+print(type(floating))
+print(type(boolean1))
+print(type(boolean2))
