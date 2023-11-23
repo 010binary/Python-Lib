44e0eafb08df279e1073672a0ea6f25d5bf4ad15
010binary <augustine32fg@gmail.com>
2023-11-23 23:58:09 +0100

    Adding inheritance

--- Classes/Basic_class.py
+++ Classes/Basic_class.py
@@ -6,12 +6,22 @@ class Human:
         self.name = name
         self.age = age
 
-    def SayName(self):
+    def SayName(self):  # Class Method
         print(f"Hey my name is {self.name} and I'm {self.age} years old")
 
 
-person1 = Human("Augustine", 20)
-person1.SayName()
+class dev(Human):  # Inheritance in Python
+    def __init__(self, name, age, job):
+        super().__init__(name, age)
+        self.job = job
+
+    def Myself(self):
+        super().SayName()
+        print(f"And i'm a {self.job} developer")
+
+
+person1 = dev("Augustine", 20, "python")
+person1.Myself()
 
 print(type(Human))
 
