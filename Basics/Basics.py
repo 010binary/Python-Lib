09b5c773020b287c7cf079e13ab8b1720066ab49
010binary <augustine32fg@gmail.com>
2023-11-26 00:02:10 +0100

    Adding the basics Comment

--- Basics/Basics.py
+++ Basics/Basics.py
@@ -1,4 +1,3 @@
-
 # Variables and Values
 name = "John"
 age = 25
@@ -22,3 +21,12 @@ example_string2 = " years old."
 
 output_string = example_string + str(example_int) + example_string2
 print(output_string)
+
+# This is a single-line comment in Python
+
+"""
+This is a multi-line comment or docstring.
+It spans across multiple lines.
+"""
+
+# Single-line comments are used for single lines, while multi-line comments or docstrings are for longer comments or documentation.
