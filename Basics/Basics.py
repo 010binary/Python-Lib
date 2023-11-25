6e48330d5451b32f1ac45fa113859b96f3a8fbc9
010binary <augustine32fg@gmail.com>
2023-11-25 23:56:32 +0100

    Adding the basics of python file

---
+++ Basics/Basics.py
@@ -0,0 +1,24 @@
+
+# Variables and Values
+name = "John"
+age = 25
+is_valid = True
+pi_value = 3.14
+
+# Reassignment of Variables
+greeting = "good morning"
+print(greeting)
+
+greeting = "hello world"
+print(greeting)
+
+# Printing to the Console
+print("Hello, welcome to Python Basics!")
+
+# Concatenation
+example_string = "you are "
+example_int = 12
+example_string2 = " years old."
+
+output_string = example_string + str(example_int) + example_string2
+print(output_string)
