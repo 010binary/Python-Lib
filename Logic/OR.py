c0f50ebe78c98864b8d3db7fed40e0250d90615a
010binary <augustine32fg@gmail.com>
2023-11-28 01:51:19 +0100

    Adding the OR Logic

---
+++ Logic/OR.py
@@ -0,0 +1,12 @@
+# Three integers to compare
+a = 10
+b = 20
+c = 15
+
+# Comparing the integers using the logical 'or' condition
+if a > b or a > c:
+    print(f"The largest number is 'a': {a}")
+elif b > c:
+    print(f"The largest number is 'b': {b}")
+else:
+    print(f"The largest number is 'c': {c}")
